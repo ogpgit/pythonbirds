a60e4b71713c22ca6085b7d63c72e140c53e8226
ogpgit <osvaldogpires@gmail.com>
2020-06-27 07:32:34 -0300

    Criado o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
 
+    def __init__(self, nome = None, idade = 36):
+        self.idade = idade
+        self.nome = nome
+
     def comprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano', 40)
     print(id(p))
     print(Pessoa.comprimentar(p))
     print(p.comprimentar())
+    print(p.nome)
+    p.nome = 'Osvaldo'
+    print(p.nome)
+    print(p.idade)
