20d807f562cd3a186171b3ea4f5743a5179828ab
ogpgit <osvaldogpires@gmail.com>
2020-07-05 18:20:35 -0300

    Criado a classe Carro

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,8 +6,8 @@ class Pessoa:
         self.nome = nome
         self.filhos = list(filhos)
 
-    def comprimentar(self):
-        return f'Olá {id(self)}'
+    def cumprimentar(self):
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -17,9 +17,16 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        return f'{super().cumprimentar()}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__ == '__main__':
-    fabio = Pessoa(nome = 'Fabio Pires')
-    luciano = Pessoa(fabio, nome='Luciano')#
+    renzo = Mutante(nome ='Renzo')
+    luciano = Homem(renzo, nome='Luciano')#
     # print(id(luciano))
     # print(f'Nome: {luciano.nome} - Idade: {luciano.idade}')
     # for filhos in luciano.filhos:
@@ -27,9 +34,10 @@ if __name__ == '__main__':
    #  luciano.sobrenome = 'Ramaho' # incluindo atributo dinâmicamente
    #  del luciano.filhos # deletando atributo dinâmicamente
 
-    # luciano.olhos = 1
-    # print(luciano.__dict__)
-    # print(fabio.__dict__)
+    luciano.olhos = 1
+    print(luciano.__dict__)
+    print(renzo.__dict__)
+
     # print(luciano.sobrenome)
 
     # print(id(Pessoa.olhos))
@@ -43,3 +51,12 @@ if __name__ == '__main__':
     # Chamada de método de classe para acessar dados da própria classe @classmethod
     # print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
 
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Homem))
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(renzo, Pessoa))
+    print(isinstance(renzo, Homem))
+    # fabio.olhos = 4
+    print(renzo.olhos)
+    print(luciano.cumprimentar())
+    print(renzo.cumprimentar())
